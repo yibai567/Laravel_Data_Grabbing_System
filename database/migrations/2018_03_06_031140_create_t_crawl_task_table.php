8e0769d776103b7ecbf987a5366d868ddb7784d6
Yu <yuwenbin.dj@gmail.com>
2018-03-06 11:37:02 +0800

    迁移文件建立

---
+++ database/migrations/2018_03_06_031140_create_t_crawl_task_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTCrawlTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t_crawl_task', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50)->nullable()->comment('任务名称');
+            $table->string('description', 200)->nullable()->comment('任务描述');
+            $table->string('resource_url', 100)->nullable()->comment('资源URL');
+            $table->unsignedTinyInteger('cron_type')->nullable()->comment('cron类型');
+            $table->text('selectors')->nullable()->comment('选择器');
+            $table->unsignedTinyInteger('status')->nullable()->comment('状态： 1、未启动 2、测试成功 3、测试失败 4、启动中 5、已停止 6、归档');
+            $table->unsignedTinyInteger('response_type')->nullable()->comment('响应类型 1、API（默认只支持）2、邮件 3、短信 4、企业微信');
+            $table->string('response_url', 100)->nullable()->comment('发送接口地址');
+            $table->string('response_params', 50)->nullable()->comment('参数');
+            $table->unsignedInteger('setting_id')->nullable()->comment('配置ID');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t_crawl_task');
+    }
+}
---
+++ database/migrations/2018_03_06_031212_create_t_crawl_task_setting_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTCrawlTaskSettingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t_crawl_task_setting', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50)->nullable()->comment('任务名称');
+            $table->string('description', 200)->nullable()->comment('任务描述');
+            $table->string('url', 100)->nullable()->comment('资源URL');
+            $table->text('selectors')->nullable()->comment('选择器：抓取规则例：{"tilte" :".result c-container", "url" : "a.href"}');
+            $table->text('keywords')->nullable()->comment('匹配关键词');
+            $table->unsignedTinyInteger('data_type')->nullable()->comment('1、html 2、json 3、xml 暂不支持');
+            $table->unsignedTinyInteger('content_type')->nullable()->comment('内容类型1、list 2、content');
+            $table->unsignedTinyInteger('is_proxy')->nullable()->comment('是否需要代理 1、需要代理 2、不需要代理');
+            $table->unsignedTinyInteger('response_type')->nullable()->comment('响应类型 1、API（默认只支持）2、邮件 3、短信 4、企业微信');
+            $table->unsignedTinyInteger('status')->nullable()->comment('状态 1、可用 2、不可用');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t_crawl_task_setting');
+    }
+}
