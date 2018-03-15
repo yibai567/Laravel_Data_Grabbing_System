a2a50cba2219e123dd3a2d89f9ee2a874110b583
Yu <yuwenbin.dj@gmail.com>
2018-03-15 16:13:45 +0800

    配置文件修改

--- config/url.php
+++ config/url.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'jinse_base_url' => 'http://api.webmagic.jinse.cn', // jinse Base API_URL
-    'jinse_internal_url' => 'http://api.webmagic.jinse.cn', // jinse Internal API_URL
-    'jinse_open_url' => 'http://api.webmagic.jinse.cn', // jinse open API_URL
+    'jinse_base_url' => env('JINSE_BASE_URL', 'http://api.webmagic.jinse.cn'), // jinse Base API_URL
+    'jinse_internal_url' => env('JINSE_INTERNAL_URL', 'http://api.webmagic.jinse.cn'), // jinse Internal API_URL
+    'jinse_open_url' => env('JINSE_OPEN_URL', 'http://api.webmagic.jinse.cn'), // jinse open API_URL
 ];
