6c34abb1f1f0ef435b03c937dd4e92def1c707b9
huangxingxing <huangxingxing@wesai.com>
2018-04-16 14:16:08 +0800

    增加任务结果和测试结果model

---
+++ app/Models/ItemTestResult.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ItemTestResult extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * 表名
+     */
+    protected $table = 't_item_result';
+
+    /**
+     * 主键
+     */
+    protected $primaryKey = "id";
+
+    /**
+     * 隐藏的字段
+     */
+    protected $hidden = [
+        "updated_at",
+        "deleted_at",
+    ];
+
+    protected $fillable = [
+        'item_id',
+        'item_run_log_id',
+        'short_content',
+        'md5_short_contents',
+        'long_content0',
+        'long_content1',
+        'images',
+        'error_message',
+        'start_at',
+        'end_at',
+        'status'
+    ];
+}
