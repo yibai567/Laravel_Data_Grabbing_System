ec742b5d30e36892386f4a130ad1a54758bf8642
Yu <yuwenbin.dj@gmail.com>
2018-04-23 12:07:57 +0800

    图片服务端 代理例子

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -44,6 +44,7 @@ class ImageService extends Service
             }
 
             $options = ['verify' => false];
+            //proxy example 'http' => 'http://username:password@199.220.220.105:2662'
             if ($isProxy) {
                 $options['proxy'] = 'tcp://127.0.0.1:8125';
             }
