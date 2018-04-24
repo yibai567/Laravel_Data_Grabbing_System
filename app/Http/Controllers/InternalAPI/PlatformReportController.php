836ca4c56bfce5e3528526dd28a56e2cd4fd2388
huangxingxing <huangxingxing@wesai.com>
2018-04-24 14:35:13 +0800

    修改结果上报接口

--- app/Http/Controllers/InternalAPI/PlatformReportController.php
+++ app/Http/Controllers/InternalAPI/PlatformReportController.php
@@ -7,6 +7,8 @@ use Log;
 use App\Services\ValidatorService;
 use App\Models\ItemRunLog;
 use App\Services\HttpService;
+use App\Services\InternalAPIService;
+;
 
 /**
  * ItemController
@@ -29,21 +31,24 @@ class PlatformReportController extends Controller
         $params = $request->all();
         ValidatorService::check($params, [
             'is_test' => 'integer|required|between:1,2',
-            'result' => 'required|string'
+            'result' => 'required|string',
         ]);
         Log::debug('[itemResultReport] 接收参数 $params : ', $params);
 
         $httpService = new HttpService();
-        $params['result'] = json_decode($params['result']);
-
+        $params['result'] = json_decode($params['result'], true);
         if ($params['is_test'] == ItemRunLog::TYPE_TEST) {
             $params['is_test'] = 1;
         } else {
             $params['is_test'] = 0;
         }
         $reportParmas = sign($params);
+
         Log::info('[itemResultReport] 请求 ' . config('url.platform_url') . ' $reportParmas : ', $reportParmas);
+
         $res = $httpService->post(config('url.platform_url'), $reportParmas, 'json');
+
+        Log::debug('[itemResultReport] $res = ' . $res);
         return $this->resObjectGet($res, 'item', $request->path());
     }
 }
