2c711b768767e4d5ec79861f2baa0c608fc22f5b
huangxingxing <huangxingxing@wesai.com>
2018-04-24 16:20:41 +0800

    修改结果上报接口

--- config/url.php
+++ config/url.php
@@ -5,4 +5,5 @@ return [
     'jinse_internal_url' => env('JINSE_INTERNAL_URL', 'http://api.webmagic.jinse.cn'), // jinse Internal API_URL
     'jinse_open_url' => env('JINSE_OPEN_URL', 'http://api.webmagic.jinse.cn'), // jinse open API_URL
     'platform_url' => env('PLARFORM_URL', ''),
+    'new_platform_url' => env('NEW_PLARFORM_URL', ''),
 ];
