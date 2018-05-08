f6d77d57bbcecb9b64d3f5037d606e61f09ec6de
huangxingxing <huangxingxing@jinse.com>
2018-05-08 18:38:24 +0800

    修改竞品分析数据入库去重，并且增加参数验证

--- app/Http/Controllers/API/V1/BlockNewsController.php
+++ app/Http/Controllers/API/V1/BlockNewsController.php
@@ -30,6 +30,24 @@ class BlockNewsController extends Controller
         $newData = [];
 
         foreach ($params['result'] as $value) {
+            $insertParams = [
+                'content_type' => BlockNews::CONTENT_TYPE_BANNER,
+                'company' => '',
+                'title' => '',
+                'md5_title' => '',
+                'md5_content' => '',
+                'content' => '',
+                'detail_url' => '',
+                'show_time' => '',
+                'read_count' => '',
+                'status' => BlockNews::STATUS_NORMAL,
+                'start_time' => '',
+                'end_time' => '',
+                'created_time' => time(),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+
+            ];
             if ($params['content_type'] == BlockNews::CONTENT_TYPE_LIVE) {
                 if (empty($value['content'])) {
                     Log::debug('[batchCreate] $value["content"] empty');
@@ -43,21 +61,20 @@ class BlockNewsController extends Controller
             }
 
             if (!empty($value['content'])) {
-                $value['content'] = trim($value['content']);
-                $value['md5_content'] = md5($value['content']);
+                $insertParams['content'] = trim($value['content']);
+                $insertParams['md5_content'] = md5($value['content']);
             }
 
             if (!empty($value['title'])) {
-                $value['title'] = trim($value['title']);
-                $value['md5_title'] = md5($value['title']);
+                $insertParams['title'] = trim($value['title']);
+                $insertParams['md5_title'] = md5($value['title']);
             }
 
             if (empty($value['title'])) {
-                $row = BlockNews::where('md5_content', $value['md5_content'])->first();
+                $row = BlockNews::where('md5_content', $insertParams['md5_content'])->first();
             } else {
-                $row = BlockNews::where('md5_title', $value['md5_title'])->first();
+                $row = BlockNews::where('md5_title', $insertParams['md5_title'])->first();
             }
-
             if (!empty($row)) {
                 if (!empty($value['read_count']) && $row->read_count != $value['read_count']) {
                     $row->read_count = $value['read_count'];
@@ -67,20 +84,60 @@ class BlockNewsController extends Controller
                 continue;
             }
 
-            $value['company'] = $params['company'];
-            $value['content_type'] = $params['content_type'];
-            $value['start_time'] = $params['start_time'];
-            $value['end_time'] = $params['end_time'];
-            $value['status'] = BlockNews::STATUS_NORMAL;
-            $value['created_time'] = time();
-            $value['created_at'] = date('Y-m-d H:i:s');
-            $value['updated_at'] = date('Y-m-d H:i:s');
-            $newData[] = $value;
+            $company = trim($params['company']);
+
+            if (mb_strlen($company) > 50) {
+                Log::debug('[batchCreate] $company 字符过长');
+                return response()->json(false);
+            }
+
+            $content_type = trim($params['content_type']);
+
+            if (!empty($content_type)) {
+                $insertParams['content_type'] = $content_type;
+            }
+            $insertParams['company'] = $company;
+            $insertParams['detail_url'] = $value['detail_url'];
+            $insertParams['show_time'] = $value['show_time'];
+            $insertParams['read_count'] = trim($value['read_count']);
+            $insertParams['start_time'] = $params['start_time'];
+            $insertParams['end_time'] = $params['end_time'];
+            $newData[] = $insertParams;
         }
 
-        if (!empty($newData)) {
+        if (empty($newData)) {
+            Log::debug('[batchCreate] $newData empty');
+            return response()->json(false);
+        }
+
+        $data = [];
+        foreach ($newData as $key => $value) {
+            $isHas = false;
+            if (!empty($data)) {
+                foreach ($data as $item) {
+                    if (!empty($value['md5_title'])) {
+                        if ($item['md5_title'] == $value['md5_title']) {
+                            $isHas = true;
+                            continue 1;
+                        }
+                    }
+                    if (!empty($value['md5_content'])) {
+                        if ($item['md5_content'] == $value['md5_content']) {
+                            $isHas = true;
+                            continue 1;
+                        }
+                    }
+                }
+            }
+
+            if (!$isHas) {
+                $data[] = $value;
+            }
+        }
+
+        if (!empty($data)) {
             try {
-                BlockNews::insert($newData);
+                BlockNews::insert($data);
             } catch (Exception $e) {
                 Log::debug('[batchCreate] error message = ' . $e->getMessage());
                 return response()->json(false);
