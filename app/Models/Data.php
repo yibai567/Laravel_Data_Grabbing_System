d3d9dbb1aab661521e8238d6b292cebe9f48f33c
huangxingxing <huangxingxing@jinse.com>
2018-05-30 18:15:44 +0800

    修改img_remaining_step 为img_task_undone

--- app/Models/Data.php
+++ app/Models/Data.php
@@ -55,7 +55,8 @@ class Data extends Model
         'author',
         'read_count',
         'thumbnail',
-        'img_remaining_step',
+        'img_task_undone',
+        'img_task_total',
         'screenshot',
         'status',
         'start_time',
@@ -85,5 +86,5 @@ class Data extends Model
         return json_decode($value, true);
     }
 
-    
+
 }
