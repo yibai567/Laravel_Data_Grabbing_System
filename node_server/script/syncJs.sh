ae2ce01878374b3209791b3eed89f2c5a00f7951
bun517 <bun517@163.com>
2018-07-24 15:20:50 +0800

    添加同步节点机器

--- node_server/script/syncJs.sh
+++ node_server/script/syncJs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #cp $1 /alidata/www/crawl/task/publish/
 scp $1 publisher@47.94.0.160:/alidata/www/crawl/task/publish/
+scp $1 publisher@47.93.15.36:/alidata/www/crawl/task/publish/
+scp $1 publisher@47.94.204.111:/alidata/www/crawl/task/publish/
