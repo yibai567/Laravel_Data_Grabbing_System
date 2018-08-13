977b31958ee786ef2eff97d36b86931e9d4d1edf
贾欢乐 <jiahuanle@jinse.com>
2018-08-13 12:19:20 +0800

    报警通知增加新接收者

--- config/alarm.php
+++ config/alarm.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'alarm_recipient' => 'liqi@jinse.com,wangbo@jinse.com,huangxingxing@jinse.com,zhangwencheng@jinse.com',
+    'alarm_recipient' => 'liqi@jinse.com,wangbo@jinse.com,huangxingxing@jinse.com,zhangwencheng@jinse.com,jiahuanle@jinse.com',
 ];
