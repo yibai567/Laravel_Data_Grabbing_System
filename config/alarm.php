1701cb18c36b66b369d56185d02789edd4603f32
贾欢乐 <jiahuanle@jinse.com>
2018-08-24 10:29:47 +0800

    优化代理监控

--- config/alarm.php
+++ config/alarm.php
@@ -1,6 +1,5 @@
 <?php
 
 return [
-    'alarm_recipient' => "liqi@jinse.com,jiahuanle@jinse.com"
-//    'alarm_recipient' => 'liqi@jinse.com,wangbo@jinse.com,huangxingxing@jinse.com,zhangwencheng@jinse.com,jiahuanle@jinse.com',
+    'alarm_recipient' => 'liqi@jinse.com,wangbo@jinse.com,huangxingxing@jinse.com,zhangwencheng@jinse.com,jiahuanle@jinse.com',
 ];
